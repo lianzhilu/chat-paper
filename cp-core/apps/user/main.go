04c2498587df63fb9434f1bb036abe21ce243c39
lianzhilu <13425936492@163.com>
2025-01-14 15:55:35 +0800

    feat: init kitex service by config

--- cp-core/apps/user/main.go
+++ cp-core/apps/user/main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"fmt"
+	"github.com/cloudwego/kitex/server"
 	"github.com/lianzhilu/chat-paper/cp-core/apps/user/service"
 	"github.com/lianzhilu/chat-paper/cp-core/kitex/kitex_gen/user/userservice"
+	"github.com/lianzhilu/chat-paper/cp-core/pkg/config"
+	"net"
+)
+
+var (
+	serviceConfig = config.GetRuntimeConfig()
+	serviceAddr   = fmt.Sprintf("%s:%s", serviceConfig.UserServiceConfig.UserServiceHost, serviceConfig.UserServiceConfig.UserServicePort)
 )
 
 func main() {
-	svc := userservice.NewServer(new(service.UserServiceImpl))
+	addr, err := net.ResolveTCPAddr("tcp", serviceAddr)
+	if err != nil {
+		panic(err)
+	}
+	svc := userservice.NewServer(
+		new(service.UserServiceImpl),
+		server.WithServiceAddr(addr),
+	)
 	if err := svc.Run(); err != nil {
 		panic(err)
 	}
