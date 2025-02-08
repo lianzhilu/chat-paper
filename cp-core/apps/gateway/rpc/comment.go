0503c16a9f73c7a9855eec8e9ce65a561368f8c2
lianzhilu <13425936492@163.com>
2025-02-08 18:04:41 +0800

    feat: init comment rpc client

--- cp-core/apps/gateway/rpc/comment.go
+++ cp-core/apps/gateway/rpc/comment.go
@@ -1,9 +1,12 @@
 package rpc
 
 import (
+	"context"
 	"github.com/cloudwego/kitex/client"
 	"github.com/cloudwego/kitex/pkg/retry"
 	etcd "github.com/kitex-contrib/registry-etcd"
+	"github.com/lianzhilu/chat-paper/cp-core/kitex/kitex_gen/base"
+	"github.com/lianzhilu/chat-paper/cp-core/kitex/kitex_gen/comment"
 	"github.com/lianzhilu/chat-paper/cp-core/kitex/kitex_gen/comment/commentservice"
 	"time"
 )
@@ -26,4 +29,20 @@ func InitCommentClient() {
 		panic(err)
 	}
 	commentClient = c
+}
+
+func CreateComment(ctx context.Context, req *comment.CreateCommentRequest) (*comment.CreateCommentResponse, error) {
+	return commentClient.CreateComment(ctx, req)
+}
+
+func GetComment(ctx context.Context, req *comment.GetCommentRequest) (*comment.CompletedComment, error) {
+	return commentClient.GetComment(ctx, req)
+}
+
+func UpdateComment(ctx context.Context, req *comment.UpdateCommonRequest) (*base.EmptyBody, error) {
+	return commentClient.UpdateComment(ctx, req)
+}
+
+func DeleteComment(ctx context.Context, req *comment.DeleteCommonRequest) (*base.EmptyBody, error) {
+	return commentClient.DeleteComment(ctx, req)
 }
